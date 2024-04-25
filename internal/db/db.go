2e9baac0b26c89c371fae1e3084a608c406779b6
IslamWalid <islamwalid83@gmail.com>
2024-04-25 20:33:48 +0200

    implement db package

--- internal/db/db.go
+++ internal/db/db.go
@@ -1,1 +1,21 @@
 package db
+
+var langMap map[int64]string
+
+func init() {
+	langMap = make(map[int64]string)
+}
+
+func GetWindowLang(id int64) (lang string, ok bool) {
+	lang, ok = langMap[id]
+
+	return lang, ok
+}
+
+func SetWindowLang(id int64, lang string) {
+	langMap[id] = lang
+}
+
+func DeleteWindow(id int64) {
+	delete(langMap, id)
+}
