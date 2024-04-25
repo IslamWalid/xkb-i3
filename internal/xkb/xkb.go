a4db83eea05752493e9d6f43bc236de1b1ff23dd
IslamWalid <islamwalid83@gmail.com>
2024-04-25 17:59:57 +0200

    implement xkb package using the underlying `xkb-switch`

--- internal/xkb/xkb.go
+++ internal/xkb/xkb.go
@@ -1,1 +1,23 @@
 package xkb
+
+import "os/exec"
+
+func CurrentKbLayout() (layout string, err error) {
+	data, err := exec.Command("xkb-switch").Output()
+	if err != nil {
+		return layout, err
+	}
+
+	layout = string(data[:len(data)-1])
+
+	return layout, err
+}
+
+func SetKbLayout(layout string) (err error) {
+	err = exec.Command("xkb-switch", "-s", layout).Run()
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
