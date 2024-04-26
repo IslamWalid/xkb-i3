94277432cca2ace65e04605bf6d6c54e094b2524
IslamWalid <islamwalid83@gmail.com>
2024-04-26 19:00:58 +0300

    create temporary implementation to options package with defautls

---
+++ internal/options/options.go
@@ -0,0 +1,24 @@
+package options
+
+const (
+	WindowMode Mode = iota
+	WorkspaceMode
+)
+
+type Mode int
+
+type Options struct {
+	Mode           Mode
+	I3Blocks       bool
+	I3BlocksSignal string
+}
+
+var DefaultOpts Options
+
+func init() {
+	DefaultOpts = Options{
+		Mode:           WindowMode,
+		I3Blocks:       false,
+		I3BlocksSignal: "SIGRTMIN+12",
+	}
+}
