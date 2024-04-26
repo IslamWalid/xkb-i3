f73806a0983046f1a89887f2018aebcff05a9ef9
IslamWalid <islamwalid83@gmail.com>
2024-04-26 18:24:53 +0300

    refactor db creation method

--- internal/db/db.go
+++ internal/db/db.go
@@ -1,23 +1,21 @@
 package db
 
-type DB struct {
-	layoutIndex map[string]int
+var layoutIndex map[string]int
+
+func init() {
+	layoutIndex = make(map[string]int)
 }
 
-func New() DB {
-	return DB{layoutIndex: make(map[string]int)}
-}
-
-func (db DB) GetLayoutIndex(id string) (index int, ok bool) {
-	index, ok = db.layoutIndex[id]
+func GetLayoutIndex(id string) (index int, ok bool) {
+	index, ok = layoutIndex[id]
 
 	return index, ok
 }
 
-func (db DB) SetLayoutIndex(id string, index int) {
-	db.layoutIndex[id] = index
+func SetLayoutIndex(id string, index int) {
+	layoutIndex[id] = index
 }
 
-func (db DB) DeleteLayoutIndex(id string) {
-	delete(db.layoutIndex, id)
+func DeleteLayoutIndex(id string) {
+	delete(layoutIndex, id)
 }
