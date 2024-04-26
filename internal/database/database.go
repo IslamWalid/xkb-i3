b2f098db24ef89f502ac036813a48113b5e75b4b
IslamWalid <islamwalid83@gmail.com>
2024-04-26 17:45:27 +0300

    unify db package schema

---
+++ internal/database/database.go
@@ -0,0 +1,23 @@
+package database
+
+type DB struct {
+	layoutIndex map[string]int
+}
+
+func New() DB {
+	return DB{layoutIndex: make(map[string]int)}
+}
+
+func (db DB) GetLayoutIndex(id string) (index int, ok bool) {
+	index, ok = db.layoutIndex[id]
+
+	return index, ok
+}
+
+func (db DB) SetLayoutIndex(id string, index int) {
+	db.layoutIndex[id] = index
+}
+
+func (db DB) DeleteLayoutIndex(id string) {
+	delete(db.layoutIndex, id)
+}
