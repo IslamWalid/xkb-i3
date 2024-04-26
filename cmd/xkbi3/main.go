43ed6297d686140e3111fd6b326ff63377232988
IslamWalid <islamwalid83@gmail.com>
2024-04-26 03:07:31 +0300

    move all window events handling to i3win package

--- cmd/xkbi3/main.go
+++ cmd/xkbi3/main.go
@@ -4,42 +4,11 @@ import (
 	"log"
 
 	"github.com/IslamWalid/xkb-i3/internal/i3win"
-	"go.i3wm.org/i3/v4"
 )
 
 func main() {
-	var curFocusID i3.NodeID
-	var err error
-
-	recv := i3.Subscribe(i3.WindowEventType)
-	defer recv.Close()
-
-
-	curFocusID, err = i3win.GetFocusedWindowID()
+	err := i3win.WindowEventHandler()
 	if err != nil {
-        log.Fatal(err)
-    }
-
-	for recv.Next() {
-		event := recv.Event().(*i3.WindowEvent)
-
-		switch event.Change {
-		case "focus":
-			err = i3win.WindowFocusEventHandler(curFocusID, event.Container.ID)
-			curFocusID = event.Container.ID
-
-		case "close":
-			err = i3win.WindowCloseEventHandler(event.Container.ID)
-			if event.Container.Focused {
-				curFocusID = 0
-			}
-
-		default:
-			continue
-		}
-
-		if err != nil {
-            log.Fatal(err)
-		}
+		log.Fatal(err)
 	}
 }
--- internal/i3win/i3win.go
+++ internal/i3win/i3win.go
@@ -6,7 +6,45 @@ import (
 	"go.i3wm.org/i3/v4"
 )
 
-func GetFocusedWindowID() (id i3.NodeID, err error) {
+func WindowEventHandler() error {
+	var curFocusID i3.NodeID
+	var err error
+
+	recv := i3.Subscribe(i3.WindowEventType)
+	defer recv.Close()
+
+	curFocusID, err = getFocusedWindowID()
+	if err != nil {
+		return err
+	}
+
+	for recv.Next() {
+		event := recv.Event().(*i3.WindowEvent)
+
+		switch event.Change {
+		case "focus":
+			err = focusEventHandler(curFocusID, event.Container.ID)
+			curFocusID = event.Container.ID
+
+		case "close":
+			err = closeEventHandler(event.Container.ID)
+			if event.Container.Focused {
+				curFocusID = 0
+			}
+
+		default:
+			continue
+		}
+
+		if err != nil {
+            return err
+		}
+	}
+
+	return nil
+}
+
+func getFocusedWindowID() (id i3.NodeID, err error) {
 	tree, err := i3.GetTree()
 	if err != nil {
 		return id, err
@@ -23,7 +61,7 @@ func GetFocusedWindowID() (id i3.NodeID, err error) {
 	return id, err
 }
 
-func WindowFocusEventHandler(oldID, curID i3.NodeID) (err error) {
+func focusEventHandler(oldID, curID i3.NodeID) (err error) {
 	var lang string
 	var ok bool
 
@@ -46,7 +84,7 @@ func WindowFocusEventHandler(oldID, curID i3.NodeID) (err error) {
 	return nil
 }
 
-func WindowCloseEventHandler(curID i3.NodeID) (err error) {
+func closeEventHandler(curID i3.NodeID) (err error) {
 	db.DeleteWindowLang(int64(curID))
 
 	return nil
