b37280b10056c276800b679fff5c2777d7946880
IslamWalid <islamwalid83@gmail.com>
2024-04-26 16:17:30 +0300

    use the new xkeyboard implementation in i3ws package

--- internal/i3ws/i3ws.go
+++ internal/i3ws/i3ws.go
@@ -2,12 +2,17 @@ package i3ws
 
 import (
 	"github.com/IslamWalid/xkb-i3/internal/db"
-	"github.com/IslamWalid/xkb-i3/internal/xkb"
+	"github.com/IslamWalid/xkb-i3/internal/xkeyboard"
 	"go.i3wm.org/i3/v4"
 )
 
 func WorkspaceEventHandler() error {
 	var err error
+
+	xkb, err := xkeyboard.New()
+	if err != nil {
+		return err
+	}
 
 	receiver := i3.Subscribe(i3.WorkspaceEventType)
 	defer receiver.Close()
@@ -16,7 +21,7 @@ func WorkspaceEventHandler() error {
 		event := receiver.Event().(*i3.WorkspaceEvent)
 		switch event.Change {
 		case "focus":
-			err = focusEventHandler(event)
+			err = focusEventHandler(xkb, event)
 		default:
 			continue
 		}
@@ -26,25 +31,22 @@ func WorkspaceEventHandler() error {
 		}
 	}
 
-    return nil
+	return nil
 }
 
-func focusEventHandler(event *i3.WorkspaceEvent) (err error) {
-	var lang string
+func focusEventHandler(xkb xkeyboard.XKeyboard, event *i3.WorkspaceEvent) (err error) {
+	var index int
 	var ok bool
 
-	lang, err = xkb.CurrentKbLayout()
+	index = xkb.GetLayoutIndex()
 	if err != nil {
 		return err
 	}
 
-	db.SetWorkspaceLang(event.Old.Name, lang)
+	db.SetWorkspaceLayoutIndex(event.Old.Name, index)
 
-	if lang, ok = db.GetWorkspaceLang(event.Current.Name); ok {
-		err = xkb.SetKbLayout(lang)
-		if err != nil {
-			return err
-		}
+	if index, ok = db.GetWorkspaceLayoutIndex(event.Current.Name); ok {
+		xkb.SetLayoutIndex(index)
 	}
 
 	return nil
