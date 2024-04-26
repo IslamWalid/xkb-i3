0b6fb126732034fd6ee4a825cbe8cf2b6e5c042e
IslamWalid <islamwalid83@gmail.com>
2024-04-26 19:10:22 +0300

    use options in main to use the desired mode

--- cmd/xkbi3/main.go
+++ cmd/xkbi3/main.go
@@ -5,10 +5,23 @@ import (
 	"os"
 
 	"github.com/IslamWalid/xkb-i3/internal/i3win"
+	"github.com/IslamWalid/xkb-i3/internal/i3ws"
+	"github.com/IslamWalid/xkb-i3/internal/options"
 )
 
 func main() {
-	err := i3win.WindowEventListner()
+	var err error
+
+	opts := options.DefaultOpts
+
+	switch opts.Mode {
+	case options.WorkspaceMode:
+		err = i3ws.WorkspaceEventListner()
+
+	case options.WindowMode:
+		err = i3win.WindowEventListner()
+	}
+
 	if err != nil {
 		fmt.Fprintln(os.Stderr, err)
 		os.Exit(1)
