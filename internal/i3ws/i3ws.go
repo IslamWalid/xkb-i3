0a75d4846c451d8b6ed2a033ec8337dbbf559b60
IslamWalid <islamwalid83@gmail.com>
2024-04-26 19:22:10 +0300

    use workspace name as its id

--- internal/i3ws/i3ws.go
+++ internal/i3ws/i3ws.go
@@ -1,14 +1,11 @@
 package i3ws
 
 import (
-	"strconv"
-
 	"github.com/IslamWalid/xkb-i3/internal/evhand"
 	"go.i3wm.org/i3/v4"
 )
 
 func WorkspaceEventListner() error {
-	var oldId, curId string
 	var err error
 
 	receiver := i3.Subscribe(i3.WorkspaceEventType)
@@ -18,9 +15,7 @@ func WorkspaceEventListner() error {
 		event := receiver.Event().(*i3.WorkspaceEvent)
 		switch event.Change {
 		case "focus":
-			oldId = strconv.FormatInt(int64(event.Old.ID), 10)
-			curId = strconv.FormatInt(int64(event.Current.ID), 10)
-			err = evhand.FocusEventHandler(oldId, curId)
+			err = evhand.FocusEventHandler(event.Old.Name, event.Current.Name)
 		}
 
 		if err != nil {
