6b476bdca6de537043e75e51b054aa7fcc82c241
IslamWalid <islamwalid83@gmail.com>
2024-04-26 19:03:22 +0300

    provide basic implementation to notify package

--- internal/notify/notify.go
+++ internal/notify/notify.go
@@ -1,5 +1,21 @@
 package notify
 
+import (
+	"fmt"
+	"os/exec"
+
+	"github.com/IslamWalid/xkb-i3/internal/options"
+)
+
 func Notify() error {
+	opts := options.DefaultOpts
+
+	if opts.I3Blocks {
+		err := exec.Command("pkill", fmt.Sprint("-%s", opts.I3BlocksSignal), "i3blocks").Run()
+		if err != nil {
+			return err
+		}
+	}
+
 	return nil
 }
