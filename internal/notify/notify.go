7309cbb745469f63a0807afc08130ea77931f1c1
IslamWalid <islamwalid83@gmail.com>
2024-05-02 00:12:06 +0300

    add todo comment in notify package

--- internal/notify/notify.go
+++ internal/notify/notify.go
@@ -11,6 +11,7 @@ func Notify() error {
 	if options.Opts.I3Blocks {
 		signal := fmt.Sprintf("-%s", options.Opts.I3BlocksSignal)
 
+        // TODO: use library to signal i3blocks with its name
 		err := exec.Command("pkill", signal, "i3blocks").Run()
 		if err != nil {
 			return err
